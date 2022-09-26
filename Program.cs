8d8c49bd18b4f04a1b377681de21b4e21b07581c
Артём Шустов < @DESKTOP-3G11VV9>
2022-09-26 21:33:21 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿//Task4
+
+int[,] nums = new int[3, 3];
+Random rnd = new Random();
+for (int i = 0; i < nums.GetLength(0); i++)
+{
+    for (int j = 0; j < nums.GetLength(1); j++)
+    {
+        nums[i, j] = rnd.Next() % 10;
+        Console.Write(nums[i, j] + " (" + i + j + ") ");
+    }
+    Console.WriteLine();
+}
+bool ChetPos;
+for (int i = 0; i < nums.GetLength(0); i++)
+{
+    for (int j = 0; j < nums.GetLength(1); j++)
+    {
+        if (i == j)
+        {
+            Console.WriteLine(nums[i, j] + " - Четное число");
+        }
+    }
+}
